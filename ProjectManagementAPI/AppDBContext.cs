5a33ad364d8936c940cbb45e0a604cb1394dd9ab
Luka <stojanicluka@gmail.com>
2025-08-06 19:06:13 +0200

    Configure DB connection
    Created AppDBContext and added DefaultConnection string to appsettings.json. Added DBContext service to Program.cs

---
+++ ProjectManagementAPI/AppDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Transactions;
+
+namespace ProjectManagementAPI
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options) { }
+
+    }
+}
