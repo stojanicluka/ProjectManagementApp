cd6173da891db384c66f1f31457d754c626fc5c5
Luka <stojanicluka@gmail.com>
2025-08-06 20:15:24 +0200

    Register Identity service in Program.cs

--- ProjectManagementAPI/Program.cs
+++ ProjectManagementAPI/Program.cs
@@ -1,5 +1,6 @@
 using ProjectManagementAPI;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDBContext>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
