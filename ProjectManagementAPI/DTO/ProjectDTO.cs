1f82e678cc0c9501c97510293a0aab76661136f6
Luka <stojanicluka@gmail.com>
2025-08-07 09:17:17 +0200

    Create Project DTO
    Created the Project DTO class used for data transfer between frontend and backend

---
+++ ProjectManagementAPI/DTO/ProjectDTO.cs
@@ -0,0 +1,15 @@
+﻿using ProjectManagementAPI.Models.Enums;
+
+namespace ProjectManagementAPI.DTO
+{
+    public class ProjectDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime Deadline { get; set; }
+        public DateTime? EndDate { get; set; }
+        public Status Status { get; set; }
+    }
+}
