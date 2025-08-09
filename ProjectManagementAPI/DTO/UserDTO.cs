42587050c3b63565fa121553c6d18825b93c116f
Luka <stojanicluka@gmail.com>
2025-08-09 16:23:34 +0200

    Create UserDTO

---
+++ ProjectManagementAPI/DTO/UserDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjectManagementAPI.DTO
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Username { get; set; }
+        public String Email { get; set; }
+
+        // Plain-text password, sent only when registering or updating user
+        public String Password { get; set; }
+
+    }
+}
