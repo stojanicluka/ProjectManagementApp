54f3fcb2a7ac7671e6e69ab04b6f620993223c82
Luka <stojanicluka@gmail.com>
2025-08-10 13:56:54 +0200

    Implement AssignRole method in UserService

--- ProjectManagementAPI/Services/UserService.cs
+++ ProjectManagementAPI/Services/UserService.cs
@@ -12,6 +12,7 @@ namespace ProjectManagementAPI.Services
         private RoleManager<IdentityRole> _roleManager;
 
         public enum RegistrationResult { SUCCESS, USERNAME_EXISTS, WRONG_EMAIL_FORMAT }
+        public enum RoleAssignmentResult { SUCCESS, USER_NOT_FOUND, ROLE_NOT_FOUND, ASSIGNMENT_ERROR }
 
         public UserService(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
@@ -39,6 +40,22 @@ namespace ProjectManagementAPI.Services
             return RegistrationResult.SUCCESS;
         }
 
+
+        public async Task<RoleAssignmentResult> AssignRole(int userID, int roleID)
+        {
+            ApplicationUser? user = await _userManager.FindByIdAsync(userID.ToString());
+            if (user == null)
+                return RoleAssignmentResult.USER_NOT_FOUND;
+
+            IdentityRole? role = await _roleManager.FindByIdAsync(roleID.ToString());
+            if (role == null)
+                return RoleAssignmentResult.ROLE_NOT_FOUND;
+
+            if (!(await _userManager.AddToRoleAsync(user, role.Name)).Succeeded)
+                return RoleAssignmentResult.ASSIGNMENT_ERROR;
+
+            return RoleAssignmentResult.SUCCESS;
+        }
 
     }
 }
