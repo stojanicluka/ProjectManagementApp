8a149e599da022a81f8114055e229e02d3680fc0
Luka <stojanicluka@gmail.com>
2025-08-11 17:47:10 +0200

    Created CreateTaskDTO

---
+++ ProjectManagementAPI/DTO/CreateTaskDTO.cs
@@ -0,0 +1,15 @@
+﻿using ProjectManagementAPI.Models.Enums;
+
+namespace ProjectManagementAPI.DTO
+{
+    public class CreateTaskDTO
+    {
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public DateTime Deadline { get; set; }
+
+        public Priority Priority { get; set; }
+        public Status Status { get; set; }
+        public int userId { get; set; }
+    }
+}
---
+++ ProjectManagementAPI/Services/TaskService.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjectManagementAPI.Services
+{
+    public class TaskService
+    {
+        private AppDBContext _dbContext;
+
+        public enum TaskCreationResult { SUCCESS, USER_NOT_FOUND, FAILUREs }
+
+        public TaskService(AppDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<TaskCreationResult> CreateTaskAsync()
+    }
+}
