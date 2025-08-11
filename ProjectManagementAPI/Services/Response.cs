163d2517945decae1cca56a857c21cbee70cd2cd
Luka <stojanicluka@gmail.com>
2025-08-11 17:39:45 +0200

    Created Response class

---
+++ ProjectManagementAPI/Services/Response.cs
@@ -0,0 +1,22 @@
+﻿namespace ProjectManagementAPI.Services
+{
+    public class Response
+    {
+        public class Error
+        {
+            public String Type { get; set; }
+            public String Message { get; set; }
+        }
+
+        public Response()
+        {
+            Errors = new List<Error>();
+        }
+
+        public bool Success { get; set; }
+        public List<Error> Errors { get; set; }
+
+        public Object? Body { get; set; }
+        
+    }
+}
