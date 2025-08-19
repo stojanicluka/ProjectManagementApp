2aec8a603f4e8b83ff416a787df1be30e6f339ab
Luka <sl220067d@student.etf.bg.ac.rs>
2025-08-19 11:10:35 +0200

    Create Login page

--- ProjectManagementAPI/wwwroot/js/login.js
+++ ProjectManagementAPI/wwwroot/js/login.js
@@ -1,25 +1,15 @@
-﻿document.getElementById('registration-form').addEventListener('submit', async function (event) {
+﻿document.getElementById('login-form').addEventListener('submit', async function (event) {
     event.preventDefault();
 
-    var first_name = document.getElementById('first-name').value;
-    var last_name = document.getElementById('last-name').value;
     var user_name = document.getElementById('username').value;
-    var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
-    var confirm_password = document.getElementById('confirm-password').value;
-
-    if (password != confirm_password)
-        document.getElementById('error-msg').textContent = 'Passwords do not match!'
 
     var data = {
-        firstName: first_name,
-        lastName: last_name,
-        email: email,
         username: user_name,
         password: password
     };
 
-    const response = await fetch('https://localhost:7239/users/register', {
+    const response = await fetch('https://localhost:7239/users/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -32,6 +22,8 @@
     console.log(result);
 
     if (result.success) {
+        sessionStorage.setItem('JWT', result.body);
+        console.log(sessionStorage.getItem('JWT'));
         document.getElementById('error-msg').textContent = 'Success!';
     } else {
         var errors = '';
